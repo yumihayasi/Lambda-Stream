2db4307820f6f073e94b2b4d72062cf21b846c24
YumiHayashi <111721334+yumihayasi@users.noreply.github.com>
2022-09-23 22:57:21 +0900

    新規Javaプロジェクト作成

---
+++ src/Chapter10.java
@@ -0,0 +1,24 @@
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Chapter10 {
+	public static void main(String[] args) throws Exception {
+		List<Task> list = new ArrayList<>();
+		list.add(new Task(LocalDate.of(2021, 10, 21), "牛乳を買う", true));
+		list.add(new Task(LocalDate.of(2021, 9, 15), "○○社面接", false));
+		list.add(new Task(LocalDate.of(2021, 12, 4), "手帳を買う", true));
+		list.add(new Task(LocalDate.of(2021, 8, 10), "散髪に行く", false));
+		list.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く", false));
+		long number =list.stream().filter(f -> f.isDone() == false).count();
+		System.out.println("未完了のタスクの個数は" + number);
+		
+		list.stream()
+		.filter(f -> f.isDone() == false)
+		.sorted()
+		.forEach(System.out::println); 
+		
+
+	}
+}
